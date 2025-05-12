9e30eb25311fe048b48632b812332186530fcf14
Dominik Kanjuh <dominik.kanjuh@outlook.com>
2025-05-12 17:50:01 +0200

    add svg support

--- script.py
+++ script.py
@@ -1,7 +1,9 @@
 import qrcode
+import qrcode.image.svg
 import os
 
-os.makedirs("images", exist_ok=True)
+os.makedirs("images/png", exist_ok=True)
+os.makedirs("images/svg", exist_ok=True)
 
 urls = {
     "main": "https://game-xpress.xfer.hr/",
@@ -9,5 +11,11 @@ urls = {
 }
 
 for name, url in urls.items():
-    img = qrcode.make(url)
-    img.save(f"images/{name}_qrcode.png")
+    # PNG
+    img_png = qrcode.make(url)
+    img_png.save(f"images/png/{name}_qrcode.png")
+
+    # SVG
+    factory = qrcode.image.svg.SvgImage
+    img_svg = qrcode.make(url, image_factory=factory)
+    img_svg.save(f"images/svg/{name}_qrcode.svg")
